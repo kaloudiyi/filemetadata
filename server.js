892b53fafdca35032c3e1023aed94e21ea5bbe23
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2017-06-01 17:46:42 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var multer = require('multer');
+var port = process.env.PORT || 3000
+
+
+var app = express();
+
+var upload = multer();
+
+app.get('/', function(req, res) {
+	res.sendFile(path.resolve('./index.html'));
+});
+
+app.post('/', upload.single('userfile'), function (req, res) {
+  console.log(req.file);
+  var obj = {size: req.file.size};
+  res.json(obj);
+})
+
+app.listen(port, function() {
+	console.log('Node.js listening on port ' + port)
+})
